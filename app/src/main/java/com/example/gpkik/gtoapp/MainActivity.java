d86b3edc6892da8bc35a07fc117bcd206edab5c0
JoseEnrique19 <1715110333@utec-tgo.edu.mx>
2018-04-03 17:23:42 -0500

    Creacion de Proyecto

---
+++ app/src/main/java/com/example/gpkik/gtoapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.gpkik.gtoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//Splash
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
